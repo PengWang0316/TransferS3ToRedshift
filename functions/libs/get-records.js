42be68b6aae1ce1102650b15b1551bfd20cef640
Kevin Wang <rocwang0316@gmail.com>
2019-02-27 22:24:06 -0800

    Added the get-records function.

---
+++ functions/libs/get-records.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const MAX_RECORD_NUMBER = 500;
+const DATABASE_TITLE_ARRAY = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g',
+];
+
+module.exports = function getRecords(contentArr) {
+  const timestamp = new Date().toISOString();
+  const records = [];
+  let record = [];
+
+  contentArr.forEach(content => {
+    const tempArr = content.split(',');
+    const tempRecord = { timestamp };
+    // Assume each content does not have more than 7 columns
+    tempArr.forEach((item, index) => {
+      tempRecord[DATABASE_TITLE_ARRAY[index]] = item;
+    });
+    record.push({ Data: Buffer.from(JSON.stringify(tempRecord)) });
+    if (record.length === MAX_RECORD_NUMBER) {
+      records.push(record);
+      record = [];
+    }
+  });
+
+  if (record.length !== 0) records.push(record);
+
+  return records;
+};
