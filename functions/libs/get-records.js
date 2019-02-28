886229c51e694023020e6633d3eea38e2a3c8540
Kevin Wang <rocwang0316@gmail.com>
2019-02-28 00:55:02 -0800

    Added the timestamp back.

--- functions/libs/get-records.js
+++ functions/libs/get-records.js
@@ -11,16 +11,18 @@ module.exports = function getRecords(contentArr) {
   let record = [];
 
   contentArr.forEach(content => {
-    const tempArr = content.split(',');
-    const tempRecord = { timestamp };
-    // Assume each content does not have more than 7 columns
-    tempArr.forEach((item, index) => {
-      tempRecord[DATABASE_TITLE_ARRAY[index]] = item;
-    });
-    record.push({ Data: Buffer.from(JSON.stringify(tempRecord)) });
-    if (record.length === MAX_RECORD_NUMBER) {
-      records.push(record);
-      record = [];
+    if (content.length !== 0) { // Skip the emplty line
+      const tempArr = content.split(',');
+      const tempRecord = { timestamp };
+      // Assume each content does not have more than 7 columns
+      tempArr.forEach((item, index) => {
+        tempRecord[DATABASE_TITLE_ARRAY[index]] = item.trim();
+      });
+      record.push({ Data: Buffer.from(JSON.stringify(tempRecord)) });
+      if (record.length === MAX_RECORD_NUMBER) {
+        records.push(record);
+        record = [];
+      }
     }
   });
 
